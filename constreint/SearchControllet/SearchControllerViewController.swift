5640fa25e1b6563de26bdcca29a6c28fbdca76ef
Yevhenii <Manilko1503gmail.com>
2019-12-05 11:03:10 +0200

    add NavigationController

---
+++ constreint/SearchControllet/SearchControllerViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SearchControllerViewController.swift
+//  constreint
+//
+//  Created by iStaff on 12/5/19.
+//  Copyright © 2019 iStaff. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    @IBAction func beacToHomeController(_ sender: Any) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+
+}
